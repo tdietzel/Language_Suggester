c45df7a9cbf1ecf8dafadec9fb600ff7922bbcdf
Trent Dietzel <dietzeltrent@gmail.com>
2023-10-20 10:01:20 -0700

    Add 5 if statements for each question.

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,9 @@ function languageCalc(e) {
     e.preventDefault();
     const colorChoice = document.querySelector("input[name='favColor']:checked");
     const animalChoice = document.querySelector("input[name='favAnimal']:checked");
+    const stateChoice = document.querySelector("input[name='favState']:checked");
+    const foodChoice = document.querySelector("input[name='favFood']:checked");
+    const languageChoice = document.querySelector("input[name='favLanguage']:checked");
 
     if (colorChoice.value === "c#") {
         console.log("C# 1")
@@ -22,5 +25,29 @@ function languageCalc(e) {
         console.log("JAVA 2")
     } else if (animalChoice.value === "python") {
         console.log("python 2")
+    }
+
+    if (stateChoice.value === "c#") {
+        console.log("C# 3")
+    } else if (stateChoice.value === "javaScript") {
+        console.log("JAVA 3")
+    } else if (stateChoice.value === "python") {
+        console.log("python 3")
+    }
+
+    if (foodChoice.value === "c#") {
+        console.log("C# 4")
+    } else if (foodChoice.value === "javaScript") {
+        console.log("JAVA 4")
+    } else if (foodChoice.value === "python") {
+        console.log("python 4")
+    }
+
+    if (languageChoice.value === "c#") {
+        console.log("C# 5")
+    } else if (languageChoice.value === "javaScript") {
+        console.log("JAVA 5")
+    } else if (languageChoice.value === "python") {
+        console.log("python 5")
     }
 }
