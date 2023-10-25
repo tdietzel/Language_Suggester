0cdef26da25bfc0be5a610b36d17ed50fffe8fab
Trent Dietzel <dietzeltrent@gmail.com>
2023-10-24 20:27:49 -0700

    Reduce code by replacing if statements.

--- js/scripts.js
+++ js/scripts.js
@@ -13,71 +13,34 @@ function languageCalc(e) {
     const stateChoice = document.querySelector("input[name='favState']:checked");
     const foodChoice = document.querySelector("input[name='favFood']:checked");
     const languageChoice = document.querySelector("input[name='favLanguage']:checked");
-    let cSharp = 0;
-    let java = 0;
-    let python = 0;
-
     reset();
 
-    if (colorChoice.value === "c#") {
-        cSharp += 1;
-    } else if (colorChoice.value === "javaScript") {
-        java += 1;
-    } else if (colorChoice.value === "python") {
-        python += 1;
+    const languageScore = {
+        csharp: 0,
+        javascript: 0,
+        python: 0
     }
 
-    if (animalChoice.value === "c#") {
-        cSharp += 1;
-    } else if (animalChoice.value === "javaScript") {
-        java += 1;
-    } else if (animalChoice.value === "python") {
-        python += 1;
-    }
+    languageScore[colorChoice.value] += 1;
+    languageScore[animalChoice.value] += 1;
+    languageScore[stateChoice.value] +=1;
+    languageScore[foodChoice.value] +=1;
+    languageScore[languageChoice.value] +=1;
 
-    if (stateChoice.value === "c#") {
-        cSharp += 1;
-    } else if (stateChoice.value === "javaScript") {
-        java += 1;
-    } else if (stateChoice.value === "python") {
-        python += 1;
-    }
-
-    if (foodChoice.value === "c#") {
-        cSharp += 1;
-    } else if (foodChoice.value === "javaScript") {
-        java += 1;
-    } else if (foodChoice.value === "python") {
-        python += 1;
-    }
-
-    if (languageChoice.value === "c#") {
-        cSharp += 1;
-    } else if (languageChoice.value === "javaScript") {
-        java += 1;
-    } else if (languageChoice.value === "python") {
-        python += 1;
-    }
-
-    if (cSharp > java && cSharp > python) {
-        console.log("Python = ", python, " JavaScript = ", java, " C# = ", cSharp)
+    if (languageScore["csharp"] > languageScore["javascript"] && languageScore["csharp"] > languageScore["python"]) {
         document.querySelector("#displayResult").innerHTML = "C# is the choice for you!";
-        document.querySelector("#displayLink").innerHTML = "<a href='https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/'>Learn more about C#</a>"
-    } else if (java > cSharp && java > python) {
-        console.log("Python = ", python, " JavaScript = ", java, " C# = ", cSharp)
+        document.querySelector("#displayLink").innerHTML = "<a href='https://learn.microsoft.com/en-us/dotnet/csharp/tour-of-csharp/'>Learn more about C#</a>";
+    } else if (languageScore["javascript"] > languageScore["csharp"] && languageScore["javascript"] > languageScore["python"]) {
         document.querySelector("#displayResult").innerHTML = "JavaScript is the choice for you!";
-        document.querySelector("#displayLink").innerHTML = "<a href='https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics'>Learn more about JavaScript</a>"
-    } else if (python > cSharp && python > java) {
-        console.log("Python = ", python, " JavaScript = ", java, " C# = ", cSharp)
+        document.querySelector("#displayLink").innerHTML = "<a href='https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics'>Learn more about JavaScript</a>";
+    } else if (languageScore["python"] > languageScore["csharp"] && languageScore["python"] > languageScore["javascript"]) {
         document.querySelector("#displayResult").innerHTML = "Python is the choice for you!";
         document.querySelector("#displayLink").innerHTML = "<a href='https://www.python.org/doc/essays/blurb/'>Learn more about Python</a>"
-    } else if (java === cSharp || java === python) {
-        console.log("Python = ", python, " JavaScript = ", java, " C# = ", cSharp)
+    } else if (languageScore["javascript"] === languageScore["csharp"] || languageScore["javascript"] === languageScore["python"]) {
         document.querySelector("#displayResult").innerHTML = "JavaScript is the choice for you!";
-        document.querySelector("#displayLink").innerHTML = "<a href='https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics'>Learn more about JavaScript</a>"
-    } else if (python === cSharp) {
-        console.log("Python = ", python, " JavaScript = ", java, " C# = ", cSharp)
+        document.querySelector("#displayLink").innerHTML = "<a href='https://developer.mozilla.org/en-US/docs/Learn/Getting_started_with_the_web/JavaScript_basics'>Learn more about JavaScript</a>";
+    } else if (languageScore["python"] === languageScore["csharp"]) {
         document.querySelector("#displayResult").innerHTML = "Python is the choice for you!";
-        document.querySelector("#displayLink").innerHTML = "<a href='https://www.python.org/doc/essays/blurb/'>Learn more about Python</a>"
+        document.querySelector("#displayLink").innerHTML = "<a href='https://www.python.org/doc/essays/blurb/'>Learn more about Python</a>";
     }
 }
